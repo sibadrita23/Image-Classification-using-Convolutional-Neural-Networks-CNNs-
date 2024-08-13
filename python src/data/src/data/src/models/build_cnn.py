7ef024cacc5c254a231063c26bdaed26c3c5a0a8
sibadrita23 <152030215+sibadrita23@users.noreply.github.com>
2024-08-13 10:09:54 +0530

    Create build_cnn.py

---
+++ python src/data/src/data/src/models/build_cnn.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+def build_cnn():
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(128, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
